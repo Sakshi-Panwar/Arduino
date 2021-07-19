c09a91cb7a3e34e5d10890990262fb3f3785ab9d
Sakshi-Panwar <sakshi19565@mechyd.ac.in>
2021-07-19 09:41:52 +0530

    breadboard

---
+++ 2.breadboard/p2.ino
@@ -0,0 +1,71 @@
+// C++ code
+//red led
+void setup()
+{
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{// led(L) open
+  digitalWrite(13, HIGH);
+  delay(500);
+  
+  digitalWrite(13, LOW);
+  delay(500);
+  
+   digitalWrite(13, HIGH);
+  delay(500);
+  
+  digitalWrite(13, LOW);
+  delay(500);
+  
+  // green led
+  
+   digitalWrite(12, HIGH);
+  delay(500);
+  
+  digitalWrite(12, LOW);
+  delay(500);
+  
+    digitalWrite(12, HIGH);
+  delay(500);
+  
+  digitalWrite(12, LOW);
+  delay(500);
+
+//yellow led
+
+digitalWrite(11, HIGH);
+  delay(500);
+digitalWrite(11, LOW);
+  delay(500);
+
+digitalWrite(11, HIGH);
+  delay(500);
+digitalWrite(11, LOW);
+  delay(500);
+
+digitalWrite(11, HIGH);
+  delay(500);
+digitalWrite(11, LOW);
+  delay(500);
+
+digitalWrite(11, HIGH);
+  delay(500);
+digitalWrite(11, LOW);
+  delay(500);
+
+digitalWrite(11, HIGH);
+  delay(500);
+digitalWrite(11, LOW);
+  delay(500);
+
+digitalWrite(11, HIGH);
+  delay(500);
+digitalWrite(11, LOW);
+  delay(500);
+  
+  
+}
+
+
